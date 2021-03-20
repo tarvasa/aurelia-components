d07b9b6ebd18c2b73b84d2074219173b65f82eeb
ferakp <akpinarferidun92@icloud.com>
2021-03-20 23:26:00 +0200

    Adds initial version of text-field component

--- src/components/text-field.js
+++ src/components/text-field.js
@@ -2,5 +2,51 @@ import {bindable} from "aurelia-framework";
 
 
 export class TextField {
+
+
+  @bindable
+  label = "Tekstikenttä";
+
+  @bindable
+  controller;
+
+  @bindable
+  required;
+
+  @bindable
+  value;
+
+  @bindable
+  errorMessage;
+
+  @bindable
+  valid;
+
+  @bindable
+  placeholder;
+
+  @bindable
+  for;
+
+  @bindable
+  empty;
+
+  @bindable
+  autoTrim;
+
+  @bindable
+  firstLetterUpperCase;
+
+  @bindable
+  allowOnlyNumbers;
+
+  @bindable
+  showClearButton;
+
+  @bindable
+  disable
+
+  @bindable
+  hideSuffixContainer;
   
 }
