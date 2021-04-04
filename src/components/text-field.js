af4de2b79a5effa258d2fd3a91007b9a215fbb6d
ferakp <akpinarferidun92@icloud.com>
2021-04-04 16:00:52 +0300

    Add styles and change attribute features

--- src/components/text-field.js
+++ src/components/text-field.js
@@ -1,4 +1,4 @@
-import { bindable } from "aurelia-framework";
+import { bindable } from 'aurelia-framework';
 
 export class TextField {
   @bindable
@@ -44,34 +44,23 @@ export class TextField {
    * @param {string} newValue the new value of value attribute
    */
   valueChanged(newValue) {
+    console.log(newValue);
     if (!this.hasBeenEdited && newValue) this.hasBeenEdited = true;
     if (
-      (this.firstLetterUpperCase === "true" ||
-        this.firstLetterUpperCase === true) &&
-      this.value &&
-      this.value[0] !== this.value[0].toUpperCase()
+      (this.firstLetterUpperCase === true)
+      && this.value
+      && this.value[0] !== this.value[0].toUpperCase()
     ) {
       this.value = this.value.charAt(0).toUpperCase() + this.value.slice(1);
     }
   }
 
-  /**
-   * Disables input element
-   * @param {string} newValue
-   */
-  disableChanged(newValue) {
-    if (!this.textFieldInputElement) return;
-    if (newValue === "true" || newValue === true)
-      this.textFieldInputElement.disabled = true;
-    else this.textFieldInputElement.disabled = false;
-  }
-
   // This function is called by icon container when ?-icon is clicked
   tooltipClicked() {
-    console.log("Tooltip clicked!");
+
   }
 
   clearButtonClicked() {
-    this.value = "";
+    this.value = '';
   }
 }
